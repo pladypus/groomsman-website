db5ab12227f517da6789768cd75183f2533ddf59
Peter Loftus <loftusp5976@gmail.com>
2026-05-02 15:40:21 -0400

    feat: finalized home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 const GroomsmanHand = () => {
   return (
-    <>
-      <h1>See My Hand</h1>
-      <ul className="grid grid-cols-3 gap-4 mx-6">
+    <div className="text-center">
+      <h1 className="text-3xl">See My Hand</h1>
+      <ul className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-6">
         <li>
           <Link
-            className="block rounded-lg bg-white dark:bg-gray-700 p-6 shadow-gray transform border border-transparent transition-transform duration-300 ease-out hover:scale-[1.02] hover:border-gray-300 hover:shadow-md"
+            className="block rounded-lg bg-white dark:bg-gray-700 p-6 shadow-gray transform border border-transparent transition-transform duration-300 ease-out hover:scale-[1.02] hover:border-gray-300 hover:shadow-md text-center"
             href="/best-man-of-the-eternal-jar"
           >
             John
@@ -15,7 +15,7 @@ const GroomsmanHand = () => {
         </li>
         <li>
           <Link
-            className="block rounded-lg bg-white dark:bg-gray-700 p-6 shadow-gray transform border border-transparent transition-transform duration-300 ease-out hover:scale-[1.02] hover:border-gray-300 hover:shadow-md"
+            className="block rounded-lg bg-white dark:bg-gray-700 p-6 shadow-gray transform border border-transparent transition-transform duration-300 ease-out hover:scale-[1.02] hover:border-gray-300 hover:shadow-md text-center"
             href="/groomsman-of-the-silver-chain"
           >
             Nick
@@ -23,14 +23,14 @@ const GroomsmanHand = () => {
         </li>
         <li>
           <Link
-            className="block rounded-lg bg-white dark:bg-gray-700 p-6 shadow-gray transform border border-transparent transition-transform duration-300 ease-out hover:scale-[1.02] hover:border-gray-300 hover:shadow-md"
+            className="block rounded-lg bg-white dark:bg-gray-700 p-6 shadow-gray transform border border-transparent transition-transform duration-300 ease-out hover:scale-[1.02] hover:border-gray-300 hover:shadow-md text-center"
             href="/the-legendary-milf-hunter-groomsman"
           >
             Kevin
           </Link>
         </li>
       </ul>
-    </>
+    </div>
   );
 };
 

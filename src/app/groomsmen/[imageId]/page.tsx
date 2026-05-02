e2d737821a7e46b975648565a1b57406c81fdd58
Peter Loftus <loftusp5976@gmail.com>
2026-05-02 12:24:12 -0400

    wip: adding metadata to set intial view size

--- src/app/groomsmen/[imageId]/page.tsx
+++ src/app/groomsmen/[imageId]/page.tsx
@@ -1,4 +1,12 @@
 import YugiohCard from "~/components/yugioh-card";
+import type { Viewport } from "next";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 
 const GroomsmenPage = () => {
   return (
